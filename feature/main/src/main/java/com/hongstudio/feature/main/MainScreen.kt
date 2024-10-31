7087333b261093e65d9f1f0927ffb0cc4d8fa6cb
Seongdeok Hong <sdhong0609@gmail.com>
2024-10-31 17:36:54 +0900

    refactor: MainNavigator 생성하여 navigation 기능 분리

--- feature/main/src/main/java/com/hongstudio/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/hongstudio/feature/main/MainScreen.kt
@@ -7,14 +7,8 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.painterResource
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.hongstudio.core.designsystem.theme.Green03
 import com.hongstudio.feature.calculator.navigation.calculatorNavGraph
 import com.hongstudio.feature.history.navigation.historyNavGraph
@@ -22,13 +16,13 @@ import com.hongstudio.feature.setting.navigation.settingNavGraph
 
 @Composable
 internal fun MainScreen() {
-    val navController = rememberNavController()
+    val navigator = rememberMainNavigator()
 
     Scaffold(
         content = { padding ->
             NavHost(
-                navController = navController,
-                startDestination = MainTab.CALCULATOR.route,
+                navController = navigator.navController,
+                startDestination = navigator.startDestination,
             ) {
                 calculatorNavGraph(padding = padding)
                 historyNavGraph(padding = padding)
@@ -37,30 +31,16 @@ internal fun MainScreen() {
         },
         bottomBar = {
             NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
                 MainTab.entries.forEach { tab ->
-                    val selected =
-                        currentDestination?.hierarchy?.any { it.hasRoute(tab.route::class) } == true
                     NavigationBarItem(
-                        selected = selected,
-                        onClick = {
-                            navController.navigate(tab.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        label = {
-                            Text(text = tab.title)
-                        },
+                        selected = navigator.currentTab == tab,
+                        onClick = { navigator.navigate(tab) },
+                        label = { Text(text = tab.title) },
                         alwaysShowLabel = false,
                         icon = {
                             Icon(
                                 painter = painterResource(tab.iconResId),
-                                tint = if (selected) Green03 else MaterialTheme.colorScheme.outline,
+                                tint = if (navigator.currentTab == tab) Green03 else MaterialTheme.colorScheme.outline,
                                 contentDescription = tab.title
                             )
                         }
