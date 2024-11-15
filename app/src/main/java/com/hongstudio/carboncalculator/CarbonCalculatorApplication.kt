dc3183e81c0077d406a02ec25a5a63ddbc03d7a3
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-16 02:08:12 +0900

    feat: Application HiltAndroidApp

--- app/src/main/java/com/hongstudio/carboncalculator/CarbonCalculatorApplication.kt
+++ app/src/main/java/com/hongstudio/carboncalculator/CarbonCalculatorApplication.kt
@@ -1,5 +1,7 @@
 package com.hongstudio.carboncalculator
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class CarbonCalculatorApplication : Application()
