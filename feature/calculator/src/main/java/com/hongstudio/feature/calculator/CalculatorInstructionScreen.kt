d5c9d2b64916e942203ebb35480d3afe653fae96
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-17 19:30:56 +0900

    feat: 계산기 사용방법 화면

---
+++ feature/calculator/src/main/java/com/hongstudio/feature/calculator/CalculatorInstructionScreen.kt
@@ -0,0 +1,89 @@
+package com.hongstudio.feature.calculator
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun CalculatorInstructionRoute(padding: PaddingValues) {
+    CalculatorInstructionScreen(padding = padding)
+}
+
+@Composable
+private fun CalculatorInstructionScreen(padding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .padding(padding)
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            "탄소발자국은 일상 속 다양한 활동에서 얼마나 많은 탄소가 배출되고 있는지 나타내는 지표입니다." +
+                    "\n\n탄소발자국 계산을 통해 우리의 일상이 기후변화에 어느 정도로 영향을 미치고 있는지 간접적으로 확인할 수 있습니다.\n"
+        )
+        Text(
+            "사용 방법\n" +
+                    "1. 아래 항목 중 계산기에 입력할 항목을 선택합니다.\n" +
+                    "2. 다음 화면에서 각 항목에 대한 사용량을 입력합니다.\n" +
+                    "3. 결과 화면에서 총 CO₂ 발생량을 확인합니다.\n"
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = { }
+                )
+                Text("전기")
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = { }
+                )
+                Text("가스")
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = { }
+                )
+                Text("수도")
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = { }
+                )
+                Text("생활 폐기물")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CalculatorInstructionScreenPreview() {
+    CalculatorInstructionScreen(padding = PaddingValues())
+}
