546d2d721a792d89898abedbfde45ce13794f03f
Seongdeok Hong <sdhong0609@gmail.com>
2024-12-23 00:05:18 +0900

    refactor: SingleButtonScreenPreview

--- feature/calculator/src/main/java/com/hongstudio/feature/calculator/component/SingleButtonScreen.kt
+++ feature/calculator/src/main/java/com/hongstudio/feature/calculator/component/SingleButtonScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hongstudio.core.designsystem.theme.White
 
@@ -133,4 +134,16 @@ internal fun SingleButtonScreen(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SingleButtonScreenPreview() {
+    SingleButtonScreen(
+        padding = PaddingValues(),
+        onButtonClick = {},
+        isButtonEnabled = true,
+        buttonTextId = android.R.string.ok,
+        content = {}
+    )
 }
